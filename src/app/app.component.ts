5dcdd08a1aa65004597232723ea9a79097c495df
sgoguen <sgoguen@gmail.com>
2019-10-23 21:45:38 -0400

    Live-view renders objects and observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { BehaviorSubject, Subscription, Observable } from "rxjs";
+import { map } from 'rxjs/operators';
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Test App';
+export class AppComponent {
+  name = "Test App";
+  liveData = {
+    x: this.createSubject("liveData", 1000),
+    y: this.createSubject("liveData", 100)
+  };
+  liveData2 = this.createSubject("liveData2", 100);
+
+  constructor() {
+  }
+
+  createSubject(name: string, ms: number) {
+    const subject = new BehaviorSubject("Initial");
+    doLoop();
+    return subject;
+
+    async function doLoop() {
+      let x = 1;
+      while (true) {
+        await delay(ms);
+        subject.next(x.toString());
+        x += 1;
+      }
+    }
+  }
+
+  identify(x: any) {
+    if(x instanceof Observable) { return 'observable'; }
+    return typeof(x);
+  }
+}
+
+function delay(ms) {
+  return new Promise((res, rej) => {
+    setTimeout(function() {
+      res();
+    }, ms);
+  });
 }
--- src/app/live-view/live-view.component.ts
+++ src/app/live-view/live-view.component.ts
@@ -1,10 +1,80 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input } from "@angular/core";
+import { BehaviorSubject, Subscription, Observable } from "rxjs";
 
 @Component({
-  selector: 'live-view',
-  template: `<h1>Live View {{value}}!</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
+  selector: "live-view",
+  templateUrl: "./live-view.component.html",
+  styleUrls: ["./live-view.component.css"]
 })
-export class LiveViewComponent  {
+export class LiveViewComponent {
   @Input() value: any;
+  @Input() maxDepth = 4;
+  private _keys: string[] | null = null;
+
+  get childDepth() {
+    return this.maxDepth - 1;
+  }
+
+  get objectType() {
+    return getObjectType(this.value);
+  }
+
+  get keys(): string[] {
+    const input = this.value;
+
+    if (this._keys !== null) {
+      return this._keys;
+    }
+    if (Array.isArray(input)) {
+      this._keys = this.getArrayKeys(input);
+    } else if (typeof input === "object") {
+      this._keys = Object.keys(input);
+    } else {
+      this._keys = [];
+    }
+    return this._keys;
+  }
+
+  private getArrayKeys(array: any[]) {
+    const keys = new Set<string>();
+    for (let i = 0; i < array.length; i++) {
+      const e = array[i];
+      if (typeof e === "object") {
+        Object.keys(e).forEach(k => {
+          keys.add(k);
+        });
+      }
+    }
+
+    return Array.from(keys);
+  }
+}
+
+export function getObjectType(i: any) {
+  if (i == null) {
+    return "null";
+  } else if (i === undefined) {
+    return "undefined";
+  }
+
+  if (i instanceof Observable) {
+    return "observable";
+  }
+
+  if (i instanceof Date) {
+    return "date";
+  } else if (typeof i === "boolean") {
+    return "bool";
+  } else if (typeof i === "number") {
+    return "number";
+  } else if (typeof i === "string") {
+    return "string";
+  } else if (typeof i === "function") {
+    return "function";
+  } else if (Array.isArray(i)) {
+    return "array";
+  } else if (typeof i === "object") {
+    return "object";
+  }
+  return "unknown";
 }
